d158435e8c638ecc5bdc12519968a7c9864c8446
DFST <mk@dfst.io>
2023-12-07 21:40:35 +0400

    plugin

--- src/api/step.ts
+++ src/api/step.ts
@@ -1,4 +1,5 @@
 import Steps from "../table/steps";
+import Jobs from "../table/jobs";
 import { StepsData } from "../model/stepsData";
 import callLambda from "../lambda/lambda";
 import { BackendPlugin, Memory } from "minanft";
@@ -16,51 +17,73 @@ export async function runStep(
     throw new Error("step is not created");
 
   const StepsTable = new Steps(process.env.STEPS_TABLE!);
-  await StepsTable.updateStatus({
-    jobId: step.jobId,
-    stepId: step.stepId,
-    status: "started",
-  });
 
-  let result: string | undefined = undefined;
-  const cacheDir = "/mnt/efs/cache";
-  await listFiles(cacheDir);
-  const cache: Cache = Cache.FileSystem(cacheDir);
+  try {
+    await StepsTable.updateStatus({
+      jobId: step.jobId,
+      stepId: step.stepId,
+      status: "started",
+    });
 
-  console.log(`Compiling...`);
-  console.time(`compiled`);
-  await plugin.compile(cache);
-  console.timeEnd(`compiled`);
-  Memory.info(`compiled`);
-  await listFiles(cacheDir);
+    let result: string | undefined = undefined;
+    const cacheDir = "/mnt/efs/cache";
+    await listFiles(cacheDir);
+    const cache: Cache = Cache.FileSystem(cacheDir);
 
-  if (step.task === "create") {
-    if (step.stepData.length !== 1)
-      throw new Error("Input length not 1 for create");
-    console.time(`created proof`);
-    result = await plugin.create(step.stepData[0]);
-    console.timeEnd(`created proof`);
-  } else if (step.task === "merge") {
-    if (step.stepData.length !== 2)
-      throw new Error("Input length not 2 for merge");
+    console.log(`Compiling...`);
+    console.time(`compiled`);
+    await plugin.compile(cache);
+    console.timeEnd(`compiled`);
+    Memory.info(`compiled`);
+    await listFiles(cacheDir);
 
-    console.time(`merged proofs`);
-    result = await plugin.merge(step.stepData[0], step.stepData[1]);
-    console.timeEnd(`merged proofs`);
-  } else throw new Error("unknown task");
-  Memory.info(`calculated proof`);
-  if (result === undefined) throw new Error("result is undefined");
+    if (step.task === "create") {
+      if (step.stepData.length !== 1)
+        throw new Error("Input length not 1 for create");
+      console.time(`created proof`);
+      result = await plugin.create(step.stepData[0]);
+      console.timeEnd(`created proof`);
+    } else if (step.task === "merge") {
+      if (step.stepData.length !== 2)
+        throw new Error("Input length not 2 for merge");
 
-  await StepsTable.updateStatus({
-    jobId: step.jobId,
-    stepId: step.stepId,
-    status: "finished",
-    result: result,
-  });
+      console.time(`merged proofs`);
+      result = await plugin.merge(step.stepData[0], step.stepData[1]);
+      console.timeEnd(`merged proofs`);
+    } else throw new Error("unknown task");
+    Memory.info(`calculated proof`);
+    if (result === undefined) throw new Error("result is undefined");
 
-  await callLambda(
-    "sequencer",
-    JSON.stringify({ task: "run", username: step.username, jobId: step.jobId })
-  );
+    await StepsTable.updateStatus({
+      jobId: step.jobId,
+      stepId: step.stepId,
+      status: "finished",
+      result: result,
+    });
+
+    await callLambda(
+      "sequencer",
+      JSON.stringify({
+        task: "run",
+        username: step.username,
+        jobId: step.jobId,
+      })
+    );
+  } catch (error) {
+    console.error("runStep error:", (<any>error).toString());
+    await StepsTable.updateStatus({
+      jobId: step.jobId,
+      stepId: step.stepId,
+      status: "failed",
+    });
+
+    const JobsTable = new Jobs(process.env.JOBS_TABLE!);
+    await JobsTable.updateStatus({
+      username: step.username,
+      jobId: step.jobId,
+      status: "failed",
+      billedDuration: step.billedDuration ?? 0,
+    });
+  }
   console.timeEnd("runStep");
 }
